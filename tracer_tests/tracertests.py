f4712e3067c90111ee7ac1ce230612a0219aae5e
Mohammed-Fadul <116148785+Mohammed-Fadul@users.noreply.github.com>
2023-02-27 14:45:18 +0100

    Update tracertests.py

--- tracer_tests/tracertests.py
+++ tracer_tests/tracertests.py
@@ -1,7 +1,10 @@
 import numpy as np
 from logging_parameters.checker import *
 
-#authored by Mohammed Fadul
+ """
+ :# authored by Mohammed Fadul
+ """
+    
 class Tracer:
     def __init__(self):
         self.d1 = .2     # drilling diameter m
